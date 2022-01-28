8944ddb6d5abf39b62f35debd1fac7402c6a01ac
suhaib079 <suhaibabdennabi33@gmail.com>
2022-01-28 18:37:49 +0200

    make a group for routes

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,25 @@ Route::get('/', function () {
     return redirect('/companies');
 });
 // Company routes
-Route::get('companies', [CompanyController::class, 'index'])->name('companies');
+Route::controller(CompanyController::class)->group(function () {
+    Route::get('companies', 'index')->name('companies');
+    Route::get('company/edit/{id}','edit')->name('companyEdit');
+    Route::post('company/edit','edditingcompany')->name('edditingcompany');
+    Route::get('company/store', 'store')->name('companystore');
+    Route::post('company/store', 'addingcompany')->name('addingcompany');
+    Route::post('company/delete','deleteingcompany')->name('deleteingcompany');
+});
 
-Route::get('company/edit/{id}',[CompanyController::class, 'edit'])->name('companyEdit');
-Route::post('company/edit',[CompanyController::class, 'edditingcompany'])->name('edditingcompany');
-
-Route::get('company/store', [CompanyController::class, 'store'])->name('companystore');
-Route::post('company/store', [CompanyController::class, 'addingcompany'])->name('addingcompany');
-
-Route::post('company/delete',[CompanyController::class, 'deleteingcompany'])->name('deleteingcompany');
 
 // Employee routes
-Route::get('employees',[EmployeeController::class, 'index'])->name('employees');
-
-Route::get('employee/edit/{id}', [EmployeeController::class, 'edit'])->name('employeeEdit');
-Route::post('employee/edit', [EmployeeController::class, 'edditingemployee'])->name('edditingemployee');
-
-Route::get('employee/store', [EmployeeController::class, 'store'])->name('employeestore');
-Route::post('employee/store', [EmployeeController::class, 'addingemployee'])->name('addingemployee');
-
-Route::post('employee/delete',[EmployeeController::class, 'deleteingemployee'])->name('deleteingemployee');
+Route::controller(EmployeeController::class)->group(function () {
+    Route::get('employees', 'index')->name('employees');
+    Route::get('employee/edit/{id}', 'edit')->name('employeeEdit');
+    Route::post('employee/edit', 'edditingemployee')->name('edditingemployee');
+    Route::get('employee/store', 'store')->name('employeestore');
+    Route::post('employee/store', 'addingemployee')->name('addingemployee');
+    Route::post('employee/delete', 'deleteingemployee')->name('deleteingemployee');
+});
 
 
 Auth::routes();
